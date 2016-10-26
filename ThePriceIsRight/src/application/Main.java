2b7af35b2c1b684e7a3a1b6b5c02d157b9c534f8
Damon Gwinn <gwinndr@clarkson.edu>
2016-10-26 18:11:56 -0400

    Added gitignore

--- ThePriceIsRight/src/application/Main.java
+++ ThePriceIsRight/src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 
 
 /**
@@ -10,7 +12,18 @@ import javafx.stage.Stage;
  */
 public class Main extends Application{
     
+    @Override
     public void start(Stage primaryStage){
+        String css = this.getClass().getResource("/resources/style.css").toExternalForm();
+        
+        VBox root = new VBox();
+        
+        Scene scene = new Scene(root, 700, 700);
+        scene.getStylesheets().add(css);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
         
     }
     
