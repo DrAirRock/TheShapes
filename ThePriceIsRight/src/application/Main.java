7b9587e0e071ba4090aa10f2600624c48716fb13
Damon Gwinn <gwinndr@clarkson.edu>
2016-10-27 12:41:34 -0400

    Added css styleing [Still in progress]

--- ThePriceIsRight/src/application/Main.java
+++ ThePriceIsRight/src/application/Main.java
@@ -1,9 +1,12 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
 
 
 /**
@@ -14,11 +17,30 @@ public class Main extends Application{
     
     @Override
     public void start(Stage primaryStage){
-       try{
-        primaryStage.setTitle("The Shapes are Right!");
-        String css = this.getClass().getResource("/application/res/style.css").toExternalForm();
+        String css = this.getClass().getResource("/application/resources/style.css").toExternalForm();
         
         VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(12);
+        
+        
+        Button btn = new Button();
+        btn.setMinWidth(100);
+        btn.setMinHeight(50);
+        btn.setFont(new Font(20));
+        btn.setText("BUTTON!");
+        
+        Button btn2 = new Button();
+        btn2.setMinWidth(100);
+        btn2.setMinHeight(50);
+        btn2.setFont(new Font(20));
+        btn2.setText("BUTTON!");
+        
+        
+        
+        
+        root.getChildren().add(btn);
+        root.getChildren().add(btn2);
         
         Scene scene = new Scene(root, 700, 700);
         scene.getStylesheets().add(css);
@@ -26,9 +48,6 @@ public class Main extends Application{
         primaryStage.setScene(scene);
         primaryStage.show();
         
-          } catch(Exception e) {
-			e.printStackTrace();
-        }
         
     }
     
